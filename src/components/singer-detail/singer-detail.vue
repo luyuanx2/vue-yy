08d470ec8bd61334102a0721070da4f912f88c3d
luyuanyuan <694436921@qq.com>
2017-12-11 01:00:47 +0800

    Initial commit

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <transation name="slide">
+    <div class="singer-detail"></div>
+  </transation>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
