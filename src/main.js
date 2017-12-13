317216009b0ff8b8ad69f1da73853ee7015a7d03
luyuanx2 <694436921@qq.com>
2017-12-13 17:51:20 +0800

    歌手详情

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
+import store from './store'
 
 import 'common/stylus/index.styl'
 fastclick.attach(document.body)
@@ -15,5 +16,6 @@ Vue.use(VueLazyLoad, {
 new Vue({
   el: '#app',
   render: h => h(App),
-  router
+  router,
+  store
 })
